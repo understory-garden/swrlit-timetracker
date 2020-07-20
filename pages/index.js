c84a529cd4ea50a76b945fca4e42fe693efdaca6
Travis Vachon <travis.vachon@gmail.com>
2020-07-20 11:51:32 -0700

    get swrlit example working

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,35 @@
 import Head from 'next/head'
+import { useThing } from "swrlit"
+import {
+  getUrlAll, getUrlOne, getStringNoLocaleOne
+} from '@itme/lit-pod'
+import { vcard, foaf } from 'rdf-namespaces'
+
+function Profile({ webId }) {
+  const { thing: profile } = useThing(webId)
+  const profileImage = profile && getUrlOne(profile, vcard.hasPhoto)
+  const name = profile && getStringNoLocaleOne(profile, foaf.name)
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={profileImage} alt={name} />
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>swrlit</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Profile webId="https://tobytoberson.inrupt.net/profile/card#me" />
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
       </footer>
 
       <style jsx>{`
