f97b70fd4dcd7ea156da2ae421de401fb7030d0a
Travis Vachon <travis.vachon@gmail.com>
2020-08-09 20:35:07 -0700

    Add tailwind and make it look a little more pretty

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,6 +25,10 @@ export function AuthButton() {
   }
 }
 
+const Loader = () => (
+  <div className="animate-spin w-6 h-6">me</div>
+)
+
 function MyProfile() {
   const { profile, save: saveProfile } = useMyProfile()
   const profileImage = profile && getUrl(profile, vcard.hasPhoto)
@@ -33,19 +37,25 @@ function MyProfile() {
   const saveNewName = () => {
     saveProfile(setStringNoLocale(profile, foaf.name, newName))
   }
-  return (
+  return profile ? (
     <div>
-      <h1>hi, {name}</h1>
-      <p>
-        <input type="text" onChange={e => setNewName(e.target.value)} />
-        <button onClick={saveNewName}>save new name</button>
-      </p>
-      <img src={profileImage} alt={name} />
+      <img className="h-48 my-6" src={profileImage} alt={name} />
+      <div className="flex flex-row">
+        <h1 className="text-xl mr-6">hi, {name}</h1>
+        <p>
+          <input type="text" onChange={e => setNewName(e.target.value)} />
+          <button onClick={saveNewName}>save new name</button>
+        </p>
+      </div>
     </div>
-  )
+  ) : (
+      <Loader />
+    )
 }
 
 export default function Home() {
+  const webId = useWebId()
+
   return (
     <div className="container">
       <Head>
@@ -53,164 +63,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <AuthButton />
-        <MyProfile />
-        <Link href="/profile/[handle]" as={`/profile/tobytoberson.inrupt.net`}>
-          <a>
-            Toby
-          </a>
-        </Link>
+      <main className="m-6">
+        {webId && (
+          <>
+            <MyProfile />
+            <h1 className="text-xl mt-12">Friends</h1>
+            <div className="flex flex-col">
+              <Link href="/profile/[handle]" as={`/profile/tobytoberson.inrupt.net`}>
+                <a>
+                  Toby
+                </a>
+              </Link>
+            </div>
+          </>
+        )}
+        <nav>
+          <AuthButton />
+        </nav>
       </main>
-
       <footer>
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-        `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-        `}</style>
     </div>
   )
 }
